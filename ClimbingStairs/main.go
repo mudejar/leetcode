75a3e7438ec133b94143571d4e7ff02ddba0367e
Bader Aljishi <mudejar6@gmail.com>
2019-11-03 15:55:33 -0800

    Added solution to climbing stairs problem on leetcode: https://leetcode.com/problems/climbing-stairs/

---
+++ ClimbingStairs/main.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.WithFields(log.Fields{
+		"2": climbStairs(2),
+		"3": climbStairs(3),
+		"45": climbStairs(45),
+	}).Println()
+}
+
+// Dynamic programming solution
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+
+	dp := []int{0,1,2}
+	for i := 3; i <= n; i++ {
+		dp = append(dp, dp[i-1] + dp[i-2])
+	}
+
+	return dp[n]
+}
