6af56bb6f6b3bfd84608072aecd4c35f3230582b
Bader Aljishi <mudejar6@gmail.com>
2019-11-07 18:36:42 -0800

    Added solution to reorder data in log files problem on leetcode: https://leetcode.com/problems/reorder-data-in-log-files/

--- LengthOfLastWord/main.go
+++ LengthOfLastWord/main.go
@@ -12,25 +12,46 @@ func main() {
 	}).Println()
 }
 
+//func lengthOfLastWord(s string) int {
+//	if len(s)== 0 {
+//		return 0
+//	}
+//
+//	wordLength := 0
+//	newWord := true
+//	for _, letter := range s {
+//		if letter == ' ' {
+//			newWord = true
+//		} else {
+//			if newWord == true {
+//				wordLength = 1
+//				newWord = false
+//			} else {
+//				wordLength++
+//			}
+//		}
+//	}
+//
+//	return wordLength
+//}
+
 func lengthOfLastWord(s string) int {
-	if len(s)== 0 {
+	if s == "" {
 		return 0
 	}
 
-	wordLength := 0
-	newWord := true
-	for _, letter := range s {
-		if letter == ' ' {
-			newWord = true
-		} else {
-			if newWord == true {
-				wordLength = 1
-				newWord = false
-			} else {
-				wordLength++
-			}
-		}
+	l := len(s)
+	for l > 0 && s[l-1] == ' ' {
+		l--
+		s = s[:l]
 	}
 
-	return wordLength
+	length := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			break
+		}
+		length++
+	}
+	return length
 }
---
+++ ReorderDataInLogFiles/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"sort"
+	"strings"
+)
+
+//Reorder the logs so that all of the letter-logs come before any digit-log.
+// The letter-logs are ordered lexicographically ignoring identifier, with
+// the identifier used in case of ties.  The digit-logs should be put in their
+// original order.
+func main() {
+
+}
+
+func reorderLogFiles(logs []string) []string {
+	letterLogs := make([]string, 0)
+	digitLogs := make([]string, 0)
+
+	// reorder the log files so that one slice is the digit logs and the other
+	// holds the letter logs
+	for _, log := range logs {
+		if isDigit(log) {
+			digitLogs = append(digitLogs, log)
+		} else {
+			letterLogs = append(letterLogs, log)
+		}
+	}
+
+
+	sort.Slice(letterLogs, func(i int, j int) bool { // This will perform a switch operation with a custom less function passed in
+		ii := strings.Index(letterLogs[i], " ") // the index of the first instance of a space character
+		ji := strings.Index(letterLogs[j], " ") // the index of the first instance of a space character
+
+		iLog := letterLogs[i][ii+1:] // all letter logs at i with the exception of the identifier
+		jLog := letterLogs[j][ji+1:] // all letter logs at j with the exception of the identifier
+		if iLog == jLog { // This compares identifiers in the case of ties
+			// This would be for a situation like the following:
+			// letterLogs[i] = "let1 art can"
+			// letterLogs[j] = "let3 art can"
+			return letterLogs[i] < letterLogs[j]
+		}
+		return iLog < jLog // This is comparing the logs but without identifiers
+	})
+
+	// append both slices, starting with the letter logs first
+	return append(letterLogs, digitLogs...)
+}
+
+func isDigit(log string) bool {
+	b := log[strings.Index(log, " ")+1]
+	return '0' <= b && b <= '9'
+}
