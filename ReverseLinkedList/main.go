2fcc9f60d4ff955cf8088700facf4e9cae362924
Bader Aljishi <mudejar6@gmail.com>
2019-11-07 19:00:19 -0800

    Added solution to reverse linked list problem on leetcode: https://leetcode.com/problems/reverse-linked-list/

---
+++ ReverseLinkedList/main.go
@@ -0,0 +1,22 @@
+package main
+
+type ListNode struct {
+	Val int
+  	Next *ListNode
+}
+
+func main() {
+
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	// 1 -> 2 -> 3 -> 4 -> 5 -> nil
+	node := reverseList(head.Next) // node = 5
+	head.Next.Next = head // 1 -> 2 -> 3 -> 4 -> 5 -> 4
+	head.Next = nil // 1 -> 2 -> 3 -> 4 -> nil -> 4
+	return node // 5 -> 4
+}
