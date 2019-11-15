185b3a216f4606db05751f6bcffd93cee0569db1
Bader Aljishi <mudejar6@gmail.com>
2019-11-14 17:04:56 -0800

    https://leetcode.com/problems/house-robber/

---
+++ HouseRobber/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	rob([]int{2,1,1,2})
+}
+
+func rob(nums []int) int {
+	prevMax := 0
+	currMax := 0
+
+	for _, num := range nums {
+		temp := currMax
+		currMax = max(prevMax + num, currMax)
+		prevMax = temp
+	}
+
+	return currMax
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
