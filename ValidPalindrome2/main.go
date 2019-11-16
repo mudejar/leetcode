e06394ac5b15596ea7f76235d34ac2b6969ccd2f
mudejar <mudejar66@gmail.com>
2019-11-16 09:31:53 -0800

    https://leetcode.com/problems/valid-palindrome-ii/

---
+++ ValidPalindrome2/main.go
@@ -0,0 +1,39 @@
+package main
+
+func main() {
+
+}
+
+func validPalindrome(s string) bool {
+	start := 0
+	end := len(s) - 1
+	for ; start < end ;{
+		if s[start] != s[end]{
+			if(isValidPalindrome(s, start + 1, end)){
+				return true
+			}
+			if(isValidPalindrome(s, start, end - 1)){
+				return true
+			}
+			return false
+		}
+
+		start++
+		end--
+	}
+
+	return true
+}
+
+
+func isValidPalindrome(s string, start int, end int) bool{
+
+	for ; start < end; {
+		if s[start] != s[end]{
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
