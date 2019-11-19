b8601ff876b2a2beee41d25c0c7cbfa594233e25
mudejar <mudejar66@gmail.com>
2019-11-18 19:53:03 -0800

    https://leetcode.com/problems/paint-house/

---
+++ PaintHouse/main.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+
+}
+
+func minCost(costs [][]int) int {
+	if len(costs) == 0 {
+		return 0
+	}
+
+	// This iteration will trickle down the existing matrix values and produce
+	// the minimum cost available for every choice by recording the current
+	// choices of values with whatever previous value can be used
+	for i := 1; i < len(costs); i++ {
+		costs[i][0] += min(costs[i-1][1],costs[i-1][2])
+		costs[i][1] += min(costs[i-1][0],costs[i-1][2])
+		costs[i][2] += min(costs[i-1][0],costs[i-1][1])
+	}
+	m := len(costs)-1
+
+	return min(min(costs[m][0],costs[m][1]),costs[m][2])
+}
+
+func min(a,b int) int {
+	if a > b {
+		return b
+	}
+
+	return a
+}
