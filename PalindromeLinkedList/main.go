c5675a92c940453ce7f07754288b872625d32373
mudejar <mudejar66@gmail.com>
2019-11-23 18:28:42 -0800

    https://leetcode.com/problems/palindrome-linked-list/

---
+++ PalindromeLinkedList/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	arr := []int{}
+	for i := head; i != nil; i = i.Next {
+		arr = append(arr, i.Val)
+	}
+
+	length := len(arr)
+	for i := 0; i < length/2; i++ {
+		if arr[i] != arr[length-i-1] {
+			return false
+		}
+	}
+
+	return true
+}
