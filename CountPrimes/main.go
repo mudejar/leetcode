189a10e9bbd6fd092bea1718ee3494cfb7f3f955
mudejar <mudejar66@gmail.com>
2019-11-24 17:19:19 -0800

    https://leetcode.com/problems/count-primes/

---
+++ CountPrimes/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+}
+
+func countPrimes(n int) int {
+	primes := make([]bool, n)
+	for i := 2; i*i < len(primes); i++ {
+		if !primes[i] {
+			for j := i; j*i < len(primes); j++ {
+				primes[i * j] = true
+			}
+		}
+	}
+	count := 0
+	for i := 2; i < len(primes); i++ {
+		if !primes[i] { count++ }
+	}
+	return count
+}
