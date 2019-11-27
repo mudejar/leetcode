8d7de1c98d8a954658ae31a1fb95ffb11bc410f4
Bader Aljishi <mudejar6@gmail.com>
2019-11-27 15:12:57 -0800

    https://leetcode.com/problems/binary-tree-postorder-traversal/

---
+++ BinaryTreePostOrderTraversal/main.go
@@ -0,0 +1,23 @@
+package main
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	arr := []int{}
+	if root == nil {
+		return arr
+	}
+
+	arr = append(arr, postorderTraversal(root.Left)...)
+	arr = append(arr, postorderTraversal(root.Right)...)
+	arr = append(arr, root.Val)
+	return arr
+}
