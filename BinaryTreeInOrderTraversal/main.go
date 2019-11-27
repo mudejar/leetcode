7c30a68090811e6373ac4328850820e486df39c3
Bader Aljishi <mudejar6@gmail.com>
2019-11-27 15:02:11 -0800

    https://leetcode.com/problems/binary-tree-inorder-traversal/

---
+++ BinaryTreeInOrderTraversal/main.go
@@ -0,0 +1,23 @@
+package main
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	arr := []int{}
+	if root == nil {
+		return arr
+	}
+
+	arr = append(arr, inorderTraversal(root.Left)...)
+	arr = append(arr, root.Val)
+	arr = append(arr, inorderTraversal(root.Right)...)
+	return arr
+}
