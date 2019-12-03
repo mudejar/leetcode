077946674893c82d178713376427bde926d9d82a
Bader Aljishi <mudejar6@gmail.com>
2019-12-03 01:03:13 -0800

    Baders-MacBook-Pro:leetcode mudejar$ git commit -m https://leetcode.com/problems/license-key-formatting/

---
+++ ContainerWithMostWater/main.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+
+}
+
+func maxArea(height []int) int {
+	totalWater := 0
+	l, r := 0, len(height)-1
+
+	for l < r {
+		width := r - l
+		ht := min(height[l], height[r])
+		totalWater = max(totalWater, width*ht)
+		if height[l] < height[r] {
+			l++
+		} else {
+			r--
+		}
+	}
+	return totalWater
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
