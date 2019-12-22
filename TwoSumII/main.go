818645f8ef5f4885ecc3d63249e9e2c4817e14d7
mudejar <mudejar66@gmail.com>
2019-12-21 21:09:55 -0800

    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/

--- TwoSumII/main.go
+++ TwoSumII/main.go
@@ -4,17 +4,35 @@ func main() {
 
 }
 
-func twoSum(numbers []int, target int) []int {
-	table := map[int]int{}
+//func twoSum(numbers []int, target int) []int {
+//	table := map[int]int{}
+//
+//	for i, num := range numbers {
+//		val, exists := table[num]
+//		if !exists {
+//			table[target-num] = i
+//		} else {
+//			return []int{val + 1, i + 1}
+//		}
+//	}
+//
+//	return nil
+//}
 
-	for i, num := range numbers {
-		val, exists := table[num]
-		if !exists {
-			table[target-num] = i
+func twoSum(numbers []int, target int) []int {
+
+	left := 0
+	right := len(numbers) - 1
+	for left < right {
+		sum := numbers[left] + numbers[right]
+		if sum == target {
+			return []int{left + 1, right + 1}
+		} else if sum < target {
+			left++
 		} else {
-			return []int{val + 1, i + 1}
+			right--
 		}
 	}
 
-	return nil
+	return []int{-1, -1}
 }
